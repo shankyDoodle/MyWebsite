d0215d178f36731900dcc47cf84ea0def4008cac
shankyDoodle <shanky4k@gmail.com>
2017-09-17 19:39:45 +0530

    contact page done

--- app/view/contact-page.jsx
+++ app/view/contact-page.jsx
@@ -6,6 +6,7 @@ var withProps = require("recompose").withProps;
 var withScriptjs = require('react-google-maps').withScriptjs;
 var withGoogleMap = require('react-google-maps').withGoogleMap;
 var GoogleMap = require('react-google-maps').GoogleMap;
+var Marker = require('react-google-maps').Marker;
 var InfoBox = require("react-google-maps").InfoWindow;
 var demoFancyMapStyles = [
     {
@@ -197,7 +198,7 @@ const StyledMapWithAnInfoBox = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCE8pmFRNX0ugYSE7wRLwra1aaEzBj7B4Q&callback=initMap",
         loadingElement: <div style={{height: `100%`}}/>,
-        containerElement: <div style={{height: `400px`}}/>,
+        containerElement: <div style={{height: `100%`}}/>,
         mapElement: <div style={{height: `100%`}}/>,
         center: {lat: 18.5072, lng: 73.9237},
     }),
@@ -209,30 +210,99 @@ const StyledMapWithAnInfoBox = compose(
         defaultCenter={props.center}
         defaultOptions={{styles: demoFancyMapStyles}}
     >
-        <InfoBox
-            defaultPosition={new google.maps.LatLng(props.center.lat, props.center.lng)}
-            options={{closeBoxURL: ``, enableEventPropagation: true}}
-        >
-            <div style={{backgroundColor: `yellow`, opacity: 0.75, padding: `12px`}}>
-                <div style={{fontSize: `16px`, fontColor: `#08233B`}}>
-                    Hello from Shashank
-                </div>
-            </div>
-        </InfoBox>
+        {/*<InfoBox
+         defaultPosition={new google.maps.LatLng(props.center.lat, props.center.lng)}
+         options={{closeBoxURL: ``, enableEventPropagation: true}}
+         >
+         <div style={{backgroundColor: `yellow`, opacity: 0.75, padding: `12px`}}>
+         <div style={{fontSize: `16px`, fontColor: `#08233B`}}>
+         Hello from Shashank
+         </div>
+         </div>
+         </InfoBox>*/}
+        <Marker
+            position={{lat: 18.5072, lng: 73.9237}}
+        />
     </GoogleMap>
 );
 
 var ContactPageView = React.createClass({
     propTypes: {},
 
+    getWordSeparatedDomArray: function (sTextString) {
+        var aDom = [];
+        var aTextString = sTextString.split(" ");
+        var iTextStringLength = aTextString.length;
+        _.forEach(aTextString, function (sWord, iIndex) {
+            aDom.push(<div className="character_shake" key={iIndex + "/" + sWord}>{sWord}</div>);
+            if (iIndex < iTextStringLength) {
+                aDom.push(<div className="character_shake" key={iIndex + "/space"}>{" "}</div>);
+            }
+        });
+        return aDom;
+    },
+
+    getHeaderText: function () {
+        var sTextString = "Contact Me";
+        return this.getWordSeparatedDomArray(sTextString);
+    },
+
+    getAddressTextDOM: function () {
+        return (
+            <div className="addressBlockContainer">
+                <div className="messageBlockContainer">
+                    <span className="addressText">Or Send me a 'Raven'...</span>
+                    <br/>
+                    <br/>
+                    <span className="mailIndicator">@ </span>
+                    <span className="addressText">A2-704, Kumar Purab, North Hadapsar, Pune, India,  411014</span>
+                </div>
+            </div>
+        );
+    },
+
+    getInTouchTextDOM: function () {
+        return (
+            <div className="getInTouchBlockContainer">
+                <div className="messageBlockContainer">
+                    <span className="addressText">Want to say </span>
+                    <span className="mailIndicator">"Hi..."</span>
+                    <span className="addressText">,</span>
+                    <br/>
+                    <span className="addressText">then why not get in touch ?</span>
+                </div>
+            </div>
+        );
+    },
+
+    getSendMeEmailDOM: function () {
+        return (
+            <div className="getSendMeMailBlockContainer">
+                <div className="messageBlockContainer">
+                    <span className="addressText">Send me E-Mail...</span>
+                    <br/>
+                    <br/>
+                    <span className="mailIndicator">@ </span>
+                    <span className="addressText">shanky4k@gmail.com</span>
+                </div>
+            </div>
+        );
+    },
+
+
     render: function () {
         return (
             <div className="contactPageContainer">
-                <div className="contactLeftBlock">
-                    <div className="informationBlock">
-                        contact me goes here
-                    </div>
-                </div>
+                {/*<div className="contactLeftBlock pageLeftBlock">
+                 <div className="informationBlock">
+                 <div className="headerInformation shakeWithH1">
+                 {this.getHeaderText()}
+                 </div>
+                 </div>
+                 </div>*/}
+                {this.getInTouchTextDOM()}
+                {this.getSendMeEmailDOM()}
+                {this.getAddressTextDOM()}
                 <div className="contactRightBlock">
                     <StyledMapWithAnInfoBox />
                 </div>
