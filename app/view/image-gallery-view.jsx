29279d4b53296bfd20b5238b438c43dba8d86e11
shankyDoodle <shanky4k@gmail.com>
2017-10-22 09:37:05 +0530

    images for art added

--- app/view/image-gallery-view.jsx
+++ app/view/image-gallery-view.jsx
@@ -3,6 +3,15 @@ var _ = require('lodash');
 var ImageGallery = require('react-image-gallery').default;
 
 var test1image = require('../scss/images/Facebook-26-Black.png');
+var CuteDreamGirlWithSpecs = require('../media/CuteDreamGirlWithSpecs.jpg');
+var BanTheFox7DeadlySins = require('../media/BanTheFox7DeadlySins.jpg');
+var BanTheFox7DeadlySins2 = require('../media/BanTheFox7DeadlySins2.png');
+var BatmanInDark = require('../media/BatmanInDark.jpg');
+var BeautifulGirlVector = require('../media/BeautifulGirlVector.jpg');
+var BirdFromCameraBlurrEffect = require('../media/BirdFromCameraBlurrEffect.jpg');
+var DeadpoolHulk = require('../media/DeadpoolHulk.jpg');
+var KakashiSenseiNaruto = require('../media/KakashiSenseiNaruto.jpg');
+var Test2Image = require('../media/preview.jpg');
 
 const PREFIX_URL = 'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/';
 
@@ -156,21 +165,64 @@ var ImageGalleryView = React.createClass({
         );
     },
 
-
-    render: function () {
-        var aImages = [
+    getItemListForArt: function () {
+        return ([
             {
                 original: test1image,
                 thumbnail: test1image,
                 originalClass: 'featured-slide',
                 thumbnailClass: 'featured-thumb',
-                description: 'Custom class for slides & thumbnails'
+                description: 'Imagine like looking at a bird from a camera lens.'
             },
             {
-                thumbnail: test1image,
-                original: test1image,
+                original: BirdFromCameraBlurrEffect,
+                thumbnail: BirdFromCameraBlurrEffect,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Imagine like looking at a bird from a camera lens.'
+            },
+            {
+                original: KakashiSenseiNaruto,
+                thumbnail: KakashiSenseiNaruto,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Copy Ninja with Sharingan - Kakashi Sensei(Naruto Anime)'
+            },
+            {
+                original: BatmanInDark,
+                thumbnail: BatmanInDark,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Dark Knight'
+            },
+            {
+                original: CuteDreamGirlWithSpecs,
+                thumbnail: CuteDreamGirlWithSpecs,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Imagined this girl in college...'
+            },
+            {
+                original: DeadpoolHulk,
+                thumbnail: DeadpoolHulk,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Deadpool annoying Hulk.'
+            },
+            {
+                original: BeautifulGirlVector,
+                thumbnail: BeautifulGirlVector,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'A small try on vector images.'
+            },
+            {
+                thumbnail: Test2Image,
+                original: Test2Image,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
                 embedUrl: 'https://www.youtube.com/watch?v=Rl3ELiPXFRo',
-                description: 'Render custom slides within the gallery',
+                description: 'Ban the fox sin from Seven Deadly Sins(Anime)',
                 renderItem: this._renderVideo.bind(this)
             },
             /*{
@@ -179,7 +231,12 @@ var ImageGalleryView = React.createClass({
              embedUrl: 'https://www.youtube.com/embed/4pSzhZ76GdM?autoplay=1&showinfo=0',
              renderItem: this._renderVideo.bind(this)
              }*/
-        ];
+        ]);
+    },
+
+    render: function () {
+        var aImages = this.getItemListForArt();
+
         return (
             <div className="imageGalleryViewContainer">
                 <ImageGallery
